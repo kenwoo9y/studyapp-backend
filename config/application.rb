eb002593d30edb9cf0526c75b1b319e45864efcd
kenwoo9y <ei.1px.9.01@gmail.com>
2023-03-06 23:22:03 +0900

    set timezone for db

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module App
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = ENV["TZ"]
 
+    config.active_record.default_timezone = :utc
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
