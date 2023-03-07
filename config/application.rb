36dcae5fefb098c49960eccbb9a6e9c808aa11ee
kenwoo9y <ei.1px.9.01@gmail.com>
2023-03-07 23:39:49 +0900

    set add_autoload_paths_to_load_path

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,8 @@ module App
 
     config.i18n.default_locale = :ja
 
+    config.add_autoload_paths_to_load_path = false
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
