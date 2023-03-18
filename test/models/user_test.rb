d1d86189985fb862d418ec32cb25e3cce327f70f
kenwoo9y <ei.1px.9.01@gmail.com>
2023-03-18 23:19:00 +0900

    add name validation test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = active_user
+  end
+
+  test "name_validation" do
+    # Input required
+    user = User.new(email: "test@example.com", password: "password")
+    user.save
+    required_msg = ["名前を入力してください"]
+    assert_equal(required_msg, user.errors.full_messages)
+
+    # Character limit(Error)
+    max = 30
+    name = "a" * (max + 1)
+    user.name = name
+    user.save
+    maxlength_msg = ["名前は30文字以内で入力してください"]
+    assert_equal(maxlength_msg, user.errors.full_messages)
+
+    # Character limit(Success)
+    name = "あ" * max
+    user.name = name
+    assert_difference("User.count", 1) do
+      user.save
+    end
+  end
 end
