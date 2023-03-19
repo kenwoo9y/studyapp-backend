064fa0086c3560e0a3950d115c71959c8a6277e6
kenwoo9y <ei.1px.9.01@gmail.com>
2023-03-19 16:12:54 +0900

    add email validation test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -27,4 +27,60 @@ class UserTest < ActiveSupport::TestCase
       user.save
     end
   end
+
+  test "email_validation" do
+    # Input required
+    user = User.new(name: "test", password: "password")
+    user.save
+    required_msg = ["メールアドレスを入力してください"]
+    assert_equal(required_msg, user.errors.full_messages)
+
+    # Character limit(Error)
+    max = 255
+    domain = "@example.com"
+    email = "a" * ((max + 1) - domain.length) + domain
+    assert max < email.length
+
+    user.email = email
+    user.save
+    maxlength_msg = ["メールアドレスは255文字以内で入力してください"]
+    assert_equal(maxlength_msg, user.errors.full_messages)
+
+    # Format check(Success)
+    ok_emails = %w(
+      A@EX.COM
+      a-_@e-x.c-o_m.j_p
+      a.a@ex.com
+      a@e.co.js
+      1.1@ex.com
+      a.a+a@ex.com
+    )
+    ok_emails.each do |email|
+      user.email = email
+      assert user.save
+    end
+
+    # Format check(Error)
+    ng_emails = %w(
+      aaa
+      a.ex.com
+      メール@ex.com
+      a~a@ex.com
+      a@|.com
+      a@ex.
+      .a@ex.com
+      a＠ex.com
+      Ａ@ex.com
+      a@?,com
+      １@ex.com
+      "a"@ex.com
+      a@ex@co.jp
+    )
+    ng_emails.each do |email|
+      user.email = email
+      user.save
+      format_msg = ["メールアドレスは不正な値です"]
+      assert_equal(format_msg, user.errors.full_messages)
+    end
+  end
 end
